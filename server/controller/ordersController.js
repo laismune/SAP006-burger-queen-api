f51c215eba6c4138429cfafc7872bcc07cab5cfb
Lais Mune <aymune@hotmail.com>
2021-10-27 16:03:47 -0300

    controllers

--- server/controller/ordersController.js
+++ server/controller/ordersController.js
@@ -1,18 +1,49 @@
-/* eslint-disable no-console */
+const db = require('../db/models');
 
-const getAllOrders = (req, res) => {
-  console.log('função getAllOrders');
-  res.send('função getAllOrders');
+const { Products } = db;
+
+const getAllOrders = async (req, res) => {
+  const getOrders = await db.Orders.findAll({
+    include: [
+      {
+        Products, as: 'Products', attributes: ['id', 'name', 'price', 'flavor', 'complement', 'type', 'sub_type'], through: { attributes: ['qtd'], as: 'details' },
+      }],
+  });
+  try {
+    return res.status(200).json({ getOrders });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 };
 
-const getOrder = (req, res) => {
-  console.log('função getOrder');
-  res.send('função getOrder');
+const getOrder = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const getOrders = await db.Orders.findOne({
+      where: { id: postId },
+      include: [
+        {
+          Products, as: 'Products', attributes: ['id', 'name', 'price', 'flavor', 'complement', 'type', 'sub_type'], through: { attributes: ['qtd'], as: 'details' },
+        }],
+    });
+    if (getOrder === null) {
+      return res.status(200).json({ message: 'Pedido inexistente' });
+    }
+    return res.status(200).json(getOrders);
+  } catch (error) {
+    next(error);
+  }
 };
 
-const postOrder = (req, res) => {
-  console.log('função postAllOrders');
-  res.send('função postAllOrders');
+const postOrder = async (req, res) => {
+  try {
+    const post = await db.Orders.create(req.body);
+    return res.status(201).json({
+      post,
+    });
+  } catch (error) {
+    return res.status(500).json({error: error.message})
+  }
 };
 
 const putOrder = (req, res) => {
